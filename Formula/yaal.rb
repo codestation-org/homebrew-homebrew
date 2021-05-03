da75a55b1d69ae5af507518d8a30738c3c3eb971
CodeStation.org <robot@codestation.org>
2021-05-03 06:30:24 +0200

    yaal - a77791d35d7a199b3a9000a2b39209837245c940

--- Formula/yaal.rb
+++ Formula/yaal.rb
@@ -3,11 +3,11 @@ class Yaal < Formula
 	homepage "https://codestation.org/"
 	license "CC-BY-NC-ND-4.0"
 	url "/Users/amok/var/bb-slave/yaal/_deploy/darwin/../..", :using => :git
-	version "0.0.44-2105020924"
+	version "0.0.44-2105030608"
 
   bottle do
     root_url "https://codestation.org/darwin/"
-    sha256 big_sur: "ff0748aa9944f548b240a71c57205a26c4da7576cbd88cce8f06bf58c07af179"
+    sha256 big_sur: "fdb67767d22f000b8ff486368bb6b62d40d664d5385a6b890e95e12ea7032ebc"
   end
 
 	depends_on "pcre"
